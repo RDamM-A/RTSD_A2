2d107df2d17b2fba6b076722ccbe49511dbf9cf8
Jose Pablo Lopez Aguado Alvarez <josepablo.lopezaguado@gmail.com>
2021-12-20 15:41:54 +0100

    FIles after remote connection

--- assignment212m.c
+++ assignment212m.c
@@ -16,14 +16,14 @@ int main()
     double accum;
 
 
-    if( clock_gettime( CLOCK_REALTIME, &start) == -1 ) {
+    if( clock_gettime( CLOCK_MONOTONIC, &start) == -1 ) {
     perror( "clock gettime" );
     }
 
     a = pthread_create( &mythread, NULL, myloop, (void*) NULL);
     pthread_join( mythread, NULL);
 
-    if( clock_gettime( CLOCK_REALTIME, &stop) == -1 ) {
+    if( clock_gettime( CLOCK_MONOTONIC, &stop) == -1 ) {
     perror( "clock gettime" );
     }
 
@@ -38,10 +38,12 @@ int main()
 void *myloop( void *ptr )
 {
     int count = 0;
+    long s;
+    long ns;
     void pthread_exit(void *retval);
+    struct timespec tp;
     while (count < 5000)
     { 
-        
     float res1= atan(tan(atan(tan(sqrt(3.1416/1)))));
     float res2= atan(tan(atan(tan(sqrt(3.1416/2)))));
     float res3= atan(tan(atan(tan(sqrt(3.1416/3)))));
@@ -57,7 +59,21 @@ void *myloop( void *ptr )
     float res13= atan(tan(atan(tan(sqrt(3.1416/13)))));
     float res14= atan(tan(atan(tan(sqrt(3.1416/14)))));
     float res15= atan(tan(atan(tan(sqrt(3.1416/15)))));
-    nanosleep((const struct timespec[]){{0, 1000000L}}, NULL);
+    
+    if (clock_gettime(CLOCK_MONOTONIC, &tp) == -1) {
+        perror("monotonic_clock");
+    };
+    //printf( "%lf and %lf\n  ", (double) tp.tv_sec, (double) tp.tv_nsec);
+    if(tp.tv_nsec>=999000000L){
+         s = tp.tv_sec +1;
+         ns = tp.tv_nsec-999000000L;
+    }
+    else{
+        s = tp.tv_sec;
+        ns = tp.tv_nsec + 1000000L;
+    }
+    clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME,(const struct timespec[]){{s,ns}},NULL);
+    //nanosleep((const struct timespec[]){{0, 1000000L}}, NULL);
     count++;
     }
 
